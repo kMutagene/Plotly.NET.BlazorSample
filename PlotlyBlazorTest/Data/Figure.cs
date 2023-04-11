72c1b16e842d8d160b7e2c5f7f0241c87b4065b2
Kevin Schneider <schneider.kev@outlook.de>
2023-04-11 11:05:57 +0200

    Add sample with JSInterop

---
+++ PlotlyBlazorTest/Data/Figure.cs
@@ -0,0 +1,9 @@
+﻿namespace PlotlyBlazorTest.Data
+{
+    public class Figure
+    {
+        public string? Traces { get; set; }
+        public string? Layout { get; set; }
+        public string? Config { get; set; }
+    }
+}
---
+++ PlotlyBlazorTest/Data/PlotService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Plotly.NET.CSharp;
+
+namespace PlotlyBlazorTest.Data
+{
+
+    public class PlotService
+    {
+
+        private Plotly.NET.GenericChart.GenericChart plot =>
+            Chart.Point<int, int, string>(
+                x: Enumerable.Range(0, 10),
+                y: Enumerable.Range(0, 10)
+            ).WithXAxisStyle<int, int, string>(
+                TitleText: "xAxis"
+            ).WithYAxisStyle<int, int, string>(
+                TitleText: "yAxis"
+            );
+
+        private JsonSerializerSettings settings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Serialize };
+
+        public Task<Figure> SamplePlot() => Task.FromResult(
+            new Figure
+            {
+                Traces = JsonConvert.SerializeObject(Plotly.NET.GenericChart.getTraces(plot), settings),
+                Layout = JsonConvert.SerializeObject(Plotly.NET.GenericChart.getLayout(plot), settings),
+                Config = JsonConvert.SerializeObject(Plotly.NET.GenericChart.getConfig(plot), settings)
+            }
+        );
+    }
+}
